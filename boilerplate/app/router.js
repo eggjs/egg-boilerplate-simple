a917e4c002d0b48dedbf42a1365baf59ff42073a
TZ <atian25@qq.com>
2016-09-02 14:08:35 +0800

    feat: redirect / to /news

--- boilerplate/app/router.js
+++ boilerplate/app/router.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = app => {
-  app.get('/', app.controller.home);
+  app.get('/home', app.controller.home);
+
+  app.redirect('/', '/news');
   app.get('/news', app.controller.news.list);
   app.get('/news/item/:id', app.controller.news.detail);
   app.get('/news/user/:id', app.controller.news.user);
