0bdba8676c3c9b46e6ddb2eedc0006f1dde31909
TZ <atian25@qq.com>
2016-10-10 09:32:22 +0800

    feat: local start options

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const isLocal = process.env.NODE_ENV !== 'production' && !process.env.EGG_SERVER_ENV;
+
 require('egg').startCluster({
   baseDir: __dirname,
-  // port: 7001, // default to 7001
-  // workers: 1, // default to cpu count
+  port: process.env.PORT || 7001, // default to 7001
+  workers: isLocal ? 1 : undefined, // default to cpu count
 });
