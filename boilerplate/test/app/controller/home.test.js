2fad72976e87e47a2cbec1a5229d54b79580f9f4
TZ <atian25@qq.com>
2017-04-12 13:48:32 +0800

    feat: remove .setup.js

--- boilerplate/test/app/controller/home.test.js
+++ boilerplate/test/app/controller/home.test.js
@@ -1,13 +1,21 @@
 'use strict';
+const mm = require('egg-mock');
+const request = require('supertest');
+const assert = require('assert');
 
 describe('test/app/controller/home.test.js', () => {
-  it('should app auto init on setup.js', () => {
-    // app is auto init at `test/.setup.js`
-    assert(app);
-    assert(mock);
-    // mock alias
-    assert(mm);
-    assert(request);
+  let app;
+  before(() => {
+    app = mm.app();
+    return app.ready();
+  });
+
+  afterEach(mm.restore);
+  after(() => app.close());
+
+  it('should assert', () => {
+    const pkg = require('../../../package.json');
+    assert(app.config.keys.startsWith(pkg.name));
   });
 
   it('should GET /', () => {
