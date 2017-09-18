ed61ea1dd903f02eef10050d80d64c71d5cdef48
TZ | 天猪 <atian25@qq.com>
2017-09-18 15:31:39 +0800

    fix: docs typo (#34)

--- boilerplate/README.zh-CN.md
+++ boilerplate/README.zh-CN.md
@@ -13,7 +13,7 @@
 ```bash
 $ npm i
 $ npm run dev
-$ open http://localhost:7001/news
+$ open http://localhost:7001/
 ```
 
 ### 部署
--- boilerplate/config/config.default.js
+++ boilerplate/config/config.default.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = {};
+  const config = exports = {};
 
-  // should change to your own
+  // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_{{keys}}';
 
   // add your config here
+  config.middleware = [];
 
   return config;
 };
