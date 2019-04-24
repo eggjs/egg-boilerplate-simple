e54e94dab818c23cadfc5db78fb8d3a915d5266f
TZ <atian25@qq.com>
2019-04-24 11:48:16 +0800

    fix: remove generator

--- boilerplate/test/app/controller/home.test.js
+++ boilerplate/test/app/controller/home.test.js
@@ -3,7 +3,7 @@
 const { app, assert } = require('egg-mock/bootstrap');
 
 describe('test/app/controller/home.test.js', () => {
-  it('should assert', function* () {
+  it('should assert', () => {
     const pkg = require('../../../package.json');
     assert(app.config.keys.startsWith(pkg.name));
 
